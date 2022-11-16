356a0cf0ab73de9be36151787a72408cfec0aac1
Harry Yu <harryyunull@gmail.com>
2022-11-16 18:00:10 -0500

    add openrelay project turn server

--- src/config/webrtc.ts
+++ src/config/webrtc.ts
@@ -4,5 +4,11 @@ export const SharerConnectionConfig: RTCConfiguration = {
     iceServers: [
         {urls: "stun:stun.stunprotocol.org:3478"},
         {urls: "stun:stun.l.google.com:19302"},
+        {
+            urls: "turn:openrelay.metered.ca:80",
+            username: "openrelayproject",
+            credential: "openrelayproject",
+            credentialType: "password"
+        }
     ],
 };
